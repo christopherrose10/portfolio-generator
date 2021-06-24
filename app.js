1ce63ea0cbb78f798cad1896ad8646fc9dd5301d
UA CR <blazer5@email.arizona.edu>
2021-06-23 18:21:08 -0700

    create gitignore, install and use inquirer for first question

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 // var commandLineArgs = process.argv;
 // console.log(commandLineArgs);
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
+
+// -------------
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
 // LESSON ONE
 
@@ -31,12 +51,21 @@ const [name, github] = profileDataArgs;
 //LESSON TWO
 
 // const generatePage = () => 'Name: Chris, Github: christopherrose10';
-
-fs.writeFile(name.toLowerCase().split(' ').join('') + '.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-})
+// -----------------
 
 
+
+
+
+// fs.writeFile(name.toLowerCase().split(' ').join('') + '.html', pageHTML, err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// })
+
+
+
+
+//-------------
 // console.log(name, github);
 // console.log(generatePage(name, github));
+//-------------
